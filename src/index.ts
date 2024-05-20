9b3bccf4c56a3c13ca26fb37df36f2485f22110a
Chokss54 <joelyang54@gmail.com>
2024-05-20 16:18:53 +1000

    added discord client login code when server start

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, {Express} from 'express'
-import dotEnvExtended from 'dotenv-extended'
+import express, {Express} from 'express';
+import dotEnvExtended from 'dotenv-extended';
+import client from './biblebot';
 
 import api from './api/index';
 
@@ -12,8 +13,14 @@ app.use(express.json());
 
 app.use("/api", api);
 
-app.listen(port, () => {
-    console.log(`⚡️[Server]: Server is running at http://localhost:${port}`);
-});
+const onServerStart = async () => {
+    app.listen(port, () => {
+        console.log(`⚡️[Server]: Server is running at http://localhost:${port}`);
+    });
+    // Initialize discord bot here...
+    await client.login();
+};
+
+onServerStart();
 
 export default app;
